de0a7b148567348cc16f7421e4fa171593e54a48
Bei Chen Liu <bei.liu@mail.mcgill.ca>
2017-03-19 05:15:10 -0400

    Finize the code for battery monitor

--- drone/battery-monitor/battery-monitor.ino
+++ drone/battery-monitor/battery-monitor.ino
@@ -1,54 +1,57 @@
 #include <Adafruit_NeoPixel.h>
-#include <avr/sleep.h>
-#include <avr/interrupt.h>
-#include <avr/io.h>
-
-#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
-#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit)) 
 
 #define NUM_LEDS 6
 #define VOLTAGE_HIGH 24.8f
 #define VOLTAGE_LOW 20.5f
 #define RESISTOR_HIGH  47000.0f
 #define RESISTOR_LOW   10000.0f
-#define VOLT_CONVERT 5.0 / 1024.0 /// (RESISTOR_LOW / (RESISTOR_HIGH + RESISTOR_LOW)) 
+#define VOLT_CONVERT 5.0 / 1024.0 / (RESISTOR_LOW / (RESISTOR_HIGH + RESISTOR_LOW)) 
 
-#define DATA_PIN 1
+#define PIN_LED 1
+#define PIN_BUTTON 2
+#define PIN_ANALOG A2
 
 // Define the array of leds
-Adafruit_NeoPixel leds = Adafruit_NeoPixel(NUM_LEDS, DATA_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel leds = Adafruit_NeoPixel(NUM_LEDS, PIN_LED, NEO_GRB + NEO_KHZ800);
+
+int timeout_count = 0;
+
+double alpha = 0.8;
+double adc_avg_value = 0;
+
 
 void setup() { 
   leds.begin();
 }
 
-void loop() { 
-  if(!digitalRead(2)){
-    int adc_read = 0;
-    for(int i = 0; i < 10 ; i++){
-      adc_read += analogRead(A2);
-    }
-    adc_read /= 10;
-    double voltage = adc_read * VOLT_CONVERT;
-    char led_on = NUM_LEDS * voltage / 5.0 + 0.5;
-    //(voltage - VOLTAGE_LOW) / (VOLTAGE_HIGH - VOLTAGE_LOW);
+
+void loop() {   
+  adc_avg_value = alpha * adc_avg_value + (1 - alpha) * analogRead(PIN_ANALOG);
+
+  if (!digitalRead(PIN_BUTTON)) {
+    timeout_count = 10;
+  }
   
-    for (int i = 0; i < NUM_LEDS; i++){
-      if( i < led_on) {
-        leds.setPixelColor(i,0x003F3F);
+  if (timeout_count > 0) {
+    timeout_count--;
+    double voltage = adc_avg_value * VOLT_CONVERT;
+
+    int led_on = NUM_LEDS * (voltage - VOLTAGE_LOW) / (VOLTAGE_HIGH - VOLTAGE_LOW) + 0.5;
+    for (int i = 0; i < NUM_LEDS; i++) {
+      if (led_on <= 0) {
+        leds.setPixelColor(i, 0x4F0000);
+      } else if (i < led_on) {
+        leds.setPixelColor(i, 0x1F4F00);
       } else {
-        leds.setPixelColor(i,0x3F1500);  
+        leds.setPixelColor(i, 0x000000);  
       }
     }
-    cli();
-    leds.show();
-    sei();
   } else {  
-    for(int i = 0; i < NUM_LEDS; i++){
-      leds.setPixelColor(i,0x000000);
+    for (int i = 0; i < NUM_LEDS; i++) {
+      leds.setPixelColor(i, 0x000000);
     }
-    cli();
-    leds.show();
-    sei();
   }
+
+  leds.show();
+  delay(100); 
 }
